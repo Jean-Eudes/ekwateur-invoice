7224ad56450eb640ab64103ac6f82ccc5d9ff19c
Jean-Eudes Couignoux <jeudes7@gmail.com>
2024-03-07 20:06:11 +0100

    introduce notion of big company.

--- app/src/main/java/io/ekwateur/invoice/domain/BusinessCustomer.java
+++ app/src/main/java/io/ekwateur/invoice/domain/BusinessCustomer.java
@@ -2,4 +2,8 @@ package io.ekwateur.invoice.domain;
 
 public record BusinessCustomer(CustomerReference reference, String siret, String businessName, long turnover) implements Customer {
 
+  public boolean isBigCompany() {
+    return turnover > 1_000_000;
+  }
+
 }
---
+++ app/src/test/java/io/ekwateur/invoice/domain/BusinessCustomerTest.java
@@ -0,0 +1,37 @@
+package io.ekwateur.invoice.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BusinessCustomerTest {
+
+  @Test
+  void should_test_if_a_company_is_small() {
+    // Given
+    CustomerReference reference = new CustomerReference("EKW12345678");
+    BusinessCustomer customer = new BusinessCustomer(reference, "jean", "durand", 10_000);
+
+    // When / Then
+    Assertions.assertThat(customer.isBigCompany()).isFalse();
+  }
+  @Test
+  void should_test_if_a_company_is_big() {
+    // Given
+    CustomerReference reference = new CustomerReference("EKW12345678");
+    BusinessCustomer customer = new BusinessCustomer(reference, "jean", "durand", 2_000_000);
+
+    // When / Then
+    Assertions.assertThat(customer.isBigCompany()).isTrue();
+  }
+  @Test
+  void should_test_if_a_company_is_small_if_turnover_is_one_million() {
+    // Given
+    CustomerReference reference = new CustomerReference("EKW12345678");
+    BusinessCustomer customer = new BusinessCustomer(reference, "jean", "durand", 1_000_000);
+
+    // When / Then
+    Assertions.assertThat(customer.isBigCompany()).isFalse();
+  }
+}
