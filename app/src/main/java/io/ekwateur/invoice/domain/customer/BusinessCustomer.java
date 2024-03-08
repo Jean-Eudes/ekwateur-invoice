1986f40b1b23ac1a5417be400e1418bed5eb6258
Jean-Eudes Couignoux <jeudes7@gmail.com>
2024-03-08 09:16:30 +0100

    prohibit a negative turnover.

--- app/src/main/java/io/ekwateur/invoice/domain/customer/BusinessCustomer.java
+++ app/src/main/java/io/ekwateur/invoice/domain/customer/BusinessCustomer.java
@@ -2,6 +2,12 @@ package io.ekwateur.invoice.domain.customer;
 
 public record BusinessCustomer(CustomerReference reference, String siret, String businessName, long turnover) implements Customer {
 
+  public BusinessCustomer {
+    if (turnover < 0) {
+      throw new IllegalArgumentException(STR."turnover can be positive : \{turnover} is negative.");
+    }
+  }
+
   public boolean isBigCompany() {
     return turnover > 1_000_000;
   }
--- app/src/test/java/io/ekwateur/invoice/domain/customer/BusinessCustomerTest.java
+++ app/src/test/java/io/ekwateur/invoice/domain/customer/BusinessCustomerTest.java
@@ -1,5 +1,8 @@
 package io.ekwateur.invoice.domain.customer;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,27 +12,38 @@ class BusinessCustomerTest {
   void should_test_if_a_company_is_small() {
     // Given
     CustomerReference reference = new CustomerReference("EKW12345678");
-    BusinessCustomer customer = new BusinessCustomer(reference, "jean", "durand", 10_000);
+    BusinessCustomer customer = new BusinessCustomer(reference, "802954785", "arcus", 10_000);
 
     // When / Then
-    Assertions.assertThat(customer.isBigCompany()).isFalse();
+    assertThat(customer.isBigCompany()).isFalse();
   }
   @Test
   void should_test_if_a_company_is_big() {
     // Given
     CustomerReference reference = new CustomerReference("EKW12345678");
-    BusinessCustomer customer = new BusinessCustomer(reference, "jean", "durand", 2_000_000);
+    BusinessCustomer customer = new BusinessCustomer(reference, "802954785", "arcus", 2_000_000);
 
     // When / Then
-    Assertions.assertThat(customer.isBigCompany()).isTrue();
+    assertThat(customer.isBigCompany()).isTrue();
   }
   @Test
   void should_test_if_a_company_is_small_if_turnover_is_one_million() {
     // Given
     CustomerReference reference = new CustomerReference("EKW12345678");
-    BusinessCustomer customer = new BusinessCustomer(reference, "jean", "durand", 1_000_000);
+    BusinessCustomer customer = new BusinessCustomer(reference, "802954785", "arcus", 1_000_000);
 
     // When / Then
-    Assertions.assertThat(customer.isBigCompany()).isFalse();
+    assertThat(customer.isBigCompany()).isFalse();
   }
+
+  @Test
+  void should_fail_if_turnover_is_negative() {
+    // Given
+    CustomerReference reference = new CustomerReference("EKW12345678");
+
+    // When / Then
+    assertThatThrownBy(() -> new BusinessCustomer(reference, "802954785", "arcus", -1)).isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("turnover can be positive : -1 is negative.");
+  }
+
 }
